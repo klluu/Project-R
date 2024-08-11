3205d38592270c5ed6b9a8826ea20a74ceffd411
Derek Sun <derekjs@uci.edu>
2024-08-10 23:19:12 -0700

    Fixed shooting
    Need to figure out how to fix the colliders

--- Assets/Scripts/Shoot Script Test.cs
+++ Assets/Scripts/Shoot Script Test.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class ShootScriptTest : MonoBehaviour
 {
-	public GameObject projectilePrefab; // Reference to the projectile prefab
-	public float projectileSpeed = 10f; // Speed of the projectile
-	public float coneAngle = 30f; // Angle of the cone in degrees
+    public GameObject projectilePrefab; // Reference to the projectile prefab
+    public float projectileSpeed = 10f; // Speed of the projectile
+    public float spreadAngle = 15f; // Angle of the spread in degrees
+    public int projectileCount = 1; // Number of projectiles to shoot per shot
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
-		{
-			ShootProjectile();
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
+        {
+            ShootProjectile();
+        }
+    }
 
-	void ShootProjectile()
-	{
-		// Instantiate the projectile at the position and rotation of the current object
-		GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-		
-		// Get the Rigidbody component of the projectile
-		Rigidbody rb = projectile.GetComponent<Rigidbody>();
-		
-		// Calculate a random direction within the cone
-		Vector3 randomDirection = Quaternion.Euler(
-			Random.Range(-coneAngle / 2, coneAngle / 2),
-			Random.Range(-coneAngle / 2, coneAngle / 2),
-			0
-		) * transform.up;
-		
-		// Add force to the projectile to move it in the random direction
-		rb.velocity = randomDirection * projectileSpeed;
+    void ShootProjectile()
+    {
+        for (int i = 0; i < projectileCount; i++)
+        {
+            // Instantiate the projectile at the position and rotation of the current object
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            
+            // Get the Rigidbody component of the projectile
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            
+            // Calculate a random direction within the horizontal spread
+            float randomYaw = Random.Range(-spreadAngle / 2, spreadAngle / 2);
+            Vector3 randomDirection = Quaternion.Euler(0, randomYaw, 0) * transform.up;
+            
+            // Add force to the projectile to move it in the random direction
+            rb.velocity = randomDirection * projectileSpeed;
 
-		// Destroy the projectile after 5 seconds
-		Destroy(projectile, 5f);
-	}
+            // Destroy the projectile after 5 seconds
+            Destroy(projectile, 5f);
+        }
+    }
 }
